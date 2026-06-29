fbc5a8fb2cb39f5802447adc0ede3692f2536fcf
Pierre Etoungha Diatta <etounghadiatta@gmail.com>
2026-06-29 23:20:51 +0200

    fix cache PWA iPhone no-store

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  headers: async () => [
+    {
+      source: '/(.*)',
+      headers: [{ key: 'Cache-Control', value: 'no-store, must-revalidate' }]
+    }
+  ],
   /* config options here */
 };
 
